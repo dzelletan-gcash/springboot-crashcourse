47f50b273b05204c944443f8a06ab548e1ced3a2
dzelletan-gcash <dzellefaith.tan@gcash.com>
2025-07-18 14:18:29 +0800

    Initialize lab6 project

---
+++ session2/lab6/src/test/java/com/example/demo/myfirstapi/model/ProductTest.java
@@ -0,0 +1,46 @@
+package com.example.demo.myfirstapi.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
